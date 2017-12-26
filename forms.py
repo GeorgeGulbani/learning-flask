91e4b5ec749b2ab7fd79e32e4c13d3903098aa28
George <George@example.com>
2017-12-26 14:12:38 +0400

    Form validation

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class SignupForm(Form):
-    first_name = StringField('First name', validators =[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    first_name = StringField('First name', validators =[DataRequired("Please enter your First name!")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your Last name!")])
+    email = StringField('Email', validators=[DataRequired("Please enter your Email!")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your Password!")])
     submit = SubmitField('Sign up')
